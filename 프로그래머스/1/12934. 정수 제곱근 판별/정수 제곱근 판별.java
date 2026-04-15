1f3205af83abf9c2906b48c405ebdfee24798e9f
mavubsa3-dev <mavubsa3@gmail.com>
2026-04-15 09:57:36 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.09 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,12 +1,12 @@
 class Solution {
     public long solution(long n) {
         long answer = 0;
-        long sqrt = (long) Math.sqrt(n);
+        long sqrt = (long)Math.sqrt(n);
         if(sqrt * sqrt == n){
             answer = (sqrt + 1) * (sqrt + 1);
-        }else
+        }else{
             answer = -1;
-        
+        }
         return answer;
-    }
+}
 }
