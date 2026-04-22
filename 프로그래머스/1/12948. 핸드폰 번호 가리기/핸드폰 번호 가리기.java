40eab9836f14862040f8eb76a6e165375144f284
mavubsa3-dev <mavubsa3@gmail.com>
2026-04-22 09:44:54 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -3,11 +3,11 @@ import java.util.*;
 class Solution {
     public String solution(String phone_number) {
         String answer = "";
-        String[] number = phone_number.split("");
+        char[] number = phone_number.toCharArray();
         for(int i=0; i<=number.length - 5; i++){
-            number[i] = "*";
+            number[i] = '*';
         }
-       
-        return String.join("", number);
+        
+        return String.valueOf(number);
     }
 }
